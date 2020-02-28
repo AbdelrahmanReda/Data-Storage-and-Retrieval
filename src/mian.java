faabd3707da2c6b7fb88dabba3164a436ba7507a
Abdelrahman Reda <boodycat09@gmail.com>
2020-02-28 23:45:39 +0200

    reflect sorting to run files

--- src/mian.java
+++ src/mian.java
@@ -38,28 +38,36 @@ public class mian {
         }
     }
     static String[] DivideInputFileIntoRuns(String Inputfilename, int runSize) {
+
         try {
             RandomAccessFile file = new RandomAccessFile("../Index.bin", "r");
+            String[] runsName = new String[512 / (runSize * 8)];
             System.out.println(file.length());
             for (int i = 0; i < file.length(); i += 512 / 8) {
                 String fileName = "src/runs/run_number_" + (i / (512 / 8) + 0) + ".bin";
+                runsName[i / (512 / 8)] = fileName;
                 RandomAccessFile runFile = new RandomAccessFile(fileName, "rw");
                 for (int j = 0; j < 8; j++) {
                     runFile.writeInt(file.readInt());
                     runFile.writeInt(file.readInt());
                 }
-                System.out.println(fileName);
+
             }
+
+            return runsName;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+
+
     }
-    public static void readRunFile() {
+    public static void readRunFile(String path) {
+
         try {
-            RandomAccessFile file = new RandomAccessFile("src/runs/run_number_0.bin", "rw");
+            RandomAccessFile file = new RandomAccessFile(path, "rw");
             for (int i = 0; i < 8; i++) {
                 System.out.print(file.readInt() + " ");
                 System.out.println(file.readInt());
@@ -70,35 +78,37 @@ public class mian {
             e.printStackTrace();
         }
     }
-    public static ArrayList<Pair> sort(ArrayList<Pair> obj){
-        for (int i = 0; i <obj.size() ; i++) {
-            for (int j = i+1; j <obj.size(); j++) {
-                if (obj.get(i).key>obj.get(j).key)
-                {
+    public static ArrayList<Pair> sort(ArrayList<Pair> obj) {
+        for (int i = 0; i < obj.size(); i++) {
+            for (int j = i + 1; j < obj.size(); j++) {
+                if (obj.get(i).key > obj.get(j).key) {
                     int temp = obj.get(i).key;
-                    obj.get(i).key=obj.get(j).key;
-                    obj.get(j).key=temp;
+                    obj.get(i).key = obj.get(j).key;
+                    obj.get(j).key = temp;
                 }
             }
         }
         return obj;
     }
-
-    static String[]  SortEachRunOnMemoryAndWriteItBack(String[] RunsFilesNames) {
-        for (int i = 0; i <RunsFilesNames.length ; i++) {
-            ArrayList <Pair> run = new ArrayList<>();
+    static String[] SortEachRunOnMemoryAndWriteItBack(String[] RunsFilesNames) {
+        for (int i = 0; i < RunsFilesNames.length; i++) {
+            ArrayList<Pair> run = new ArrayList<>();
             try {
-                RandomAccessFile file = new RandomAccessFile(RunsFilesNames[i],"rw");
-                for (int j = 0; j <file.length()/8 ; j++){
+                RandomAccessFile file = new RandomAccessFile(RunsFilesNames[i], "rw");
+                for (int j = 0; j < file.length() / 8; j++) {
                     Pair pair = new Pair();
-                    pair.key=file.readInt();
-                    pair.byteOffset=file.readInt();
+                    pair.key = file.readInt();
+                    pair.byteOffset = file.readInt();
                     run.add(pair);
                 }
-                run=sort(run);
-                for (int ii = 0; ii <run.size() ; ii++) {
-                    System.out.println(run.get(ii).key);
+                run = sort(run);
+                file.seek(0);
+                for (int ii = 0; ii < run.size(); ii++) {
+
+                    file.writeInt(run.get(ii).key);
+                    file.writeInt(run.get(ii).byteOffset);
                 }
+                file.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -107,17 +117,14 @@ public class mian {
         }
         return null;
     }
-    void DoKWayMergeAndWriteASortedFile(String[] SortedRunsNames, int K, String Sortedfilename){
+    void DoKWayMergeAndWriteASortedFile(String[] SortedRunsNames, int K, String Sortedfilename) {
     }
     int BinarySearchOnSortedFile(String Sortedfilename, int RecordKey) {
         return 0;
     }
     public static void main(String args[]) throws IOException {
-        String [] runFiles =  new String[1];
-        runFiles[0]="src/runs/run_number_0.bin";
-        SortEachRunOnMemoryAndWriteItBack(runFiles);
-
-
+        String[] Runs = DivideInputFileIntoRuns("m", 8);
+        SortEachRunOnMemoryAndWriteItBack(Runs);
 
     }
 }
