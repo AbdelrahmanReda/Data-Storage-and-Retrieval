dd17efaab22142a3bd9a3609a434fa8352d4ec19
Abdelrahman Reda <boodycat09@gmail.com>
2020-02-28 16:10:52 +0200

    fix consequential processing

--- src/mian.java
+++ src/mian.java
@@ -4,40 +4,53 @@ import java.util.Arrays;
 
 public class mian
  {
-    
-    
-static ArrayList<Integer>  coseqentialProcessing(ArrayList<Integer> runLeft , ArrayList<Integer> runRight){
-    ArrayList<Integer> fullRun = new ArrayList<>();
-       int counter_left = 0;
-       int counter_right=0;
-        for (int j = 0; j <runLeft.size()+runRight.size()-1 ; j++) {
-            System.out.println(counter_left+" "+counter_right);
-            if (runLeft.get(counter_left)<runRight.get(counter_right))
-            {
-                System.out.println("right is greater");
-                fullRun.add(runLeft.get(counter_left));
-                if ((counter_left+1) != runLeft.size())
-                counter_left++;
-            }
-            else
-            {
-                System.out.println("left is greater");
-                fullRun.add(runRight.get(counter_right));
-                if ((counter_right+1) != runRight.size())
-                counter_right++;
-            }
-        }
-    
-    System.out.println(fullRun);
-    
-    return null;
-    }
+     public static void cosequentialProcessing(ArrayList<Integer> arrayListOne ,ArrayList<Integer> arrayListTwo  ){
+         ArrayList <Integer> result = new ArrayList<>();
+         int a1_counter = 0;
+         int a2_counter = 0 ;
+         while (a1_counter!=arrayListOne.size() && a2_counter!=arrayListTwo.size() )
+         {
+             while (a2_counter!=arrayListTwo.size())
+             {
+                 if (arrayListOne.get(a1_counter)<arrayListTwo.get(a2_counter))
+                 {
+                     System.out.println("a1 less than");
+                     result.add(arrayListOne.get(a1_counter));
+                     a1_counter++;
+                     break;
+                 }
+                 else
+                 {
+                     System.out.println("a2 less than");
+                     result.add(arrayListTwo.get(a2_counter));
+                     a2_counter++;
+                 }
+             }
+             try {
+                 Thread.sleep(300);
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+         System.out.println("aoutded");
+         while (a1_counter!=arrayListOne.size())
+         {
+             result.add(arrayListOne.get(a1_counter));
+             a1_counter++;
+         }
+         while (a2_counter!=arrayListTwo.size())
+         {
+             result.add(arrayListTwo.get(a2_counter));
+             a2_counter++;
+         }
+         System.out.println("resultis"+result);
+     }
     
     public static void main (String args []){
       
         ArrayList<Integer> runLeft = new ArrayList<>(Arrays.asList(1,3,5));
         ArrayList<Integer> runRight = new ArrayList<>(Arrays.asList(2));
-        coseqentialProcessing(runLeft,runRight);
+        cosequentialProcessing(runLeft,runRight);
     
     
     
